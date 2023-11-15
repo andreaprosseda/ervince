b512f09f3f3c9de2a70d362f1607f00617de97bd
Andrea Prosseda <andreaprosseda@gmail.com>
2023-11-15 15:57:13 +0100

    Inizializzo il nuovo progetto Spring Boot

---
+++ src/test/java/vincenzone/GeneralArchetypeApplicationTests.java
@@ -0,0 +1,13 @@
+package vincenzone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneralArchetypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
